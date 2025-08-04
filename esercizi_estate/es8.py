46e4c02fa71de5e1ce59ed9da59bc0c7a05c4254
xxxNdr <andrea1987andre@gmail.com>
2025-08-04 19:08:54 +0200

    es8 completato

--- esercizi_estate/es8.py
+++ esercizi_estate/es8.py
@@ -1,1 +1,14 @@
 def aggiornaImpostazioni(impostazioniAttuali:dict, **nuoveImpostazioni):
+    impostazioniAttuali.update(nuoveImpostazioni)
+    output = ""
+    for k, v in impostazioniAttuali.items():
+        output += f"{k}: {v}\n" 
+    return output
+
+
+rubrica = {'nome': 'Alice',
+           'eta': 30}
+
+risultato = aggiornaImpostazioni(rubrica, citta='Bologna', eta=38, gruppo_sanguineo='AB+')
+
+print(risultato)
