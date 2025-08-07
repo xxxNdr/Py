383b5094a82841fd4667a268502a3179dc208902
xxxNdr <andrea1987andre@gmail.com>
2025-08-07 09:55:07 +0200

    verifica python quasi-completata

--- verifica_08_2025/classe/evento.py
+++ verifica_08_2025/classe/evento.py
@@ -12,29 +12,35 @@ class Evento:
     
     def prenota(s, numero_posti):
         if numero_posti <= 0:
-             print("Richiesta impossibile")
+             print("Richiesta impossibile\n")
              return False
         
         disponibili = s.posti_disponibili()
         if numero_posti <= disponibili:
                 s.posti_prenotati += numero_posti
-                print(f"Hai correttamente prenotato {numero_posti} posti")
+                print(f"Hai correttamente prenotato {numero_posti} posti\n")
                 return True
         else:
-            print(f"Non ci sono abbastanza posti diponibili: {disponibili} / richiesti: {numero_posti}")
+            print(f"Non ci sono abbastanza posti diponibili: {disponibili} / richiesti: {numero_posti}\n")
             return False
         
     def annulla_prenotazione(s, numero_posti):
          if numero_posti <= s.posti_prenotati:
               s.posti_prenotati -= numero_posti
-              print(f"Hai correttamente annullato {numero_posti} prenotazioni")
+              print(f"Hai correttamente annullato {numero_posti} prenotazioni\n")
               return True
          else:
-              print("Non puoi annullare più posti di quanti ne hai prenotati")
+              print("Non puoi annullare più posti di quanti ne hai prenotati\n")
               return False
     
     def calcola_incasso(s):
-         
+        tot = s.posti_prenotati * s.prezzo
+        print(f"L'incasso totale per l'evento di {s.nome} è di {tot} €\n")
+
+    def ottieni_info_evento(s):
+        tot = s.posti_prenotati * s.prezzo
+        potenziale = s.posti_disponibili() * s.prezzo
+        print(f"Evento: {s.nome}\nData: {s.data}\nPosti totali: {s.posti_totali}\nPosti prenotati: {s.posti_prenotati}\nIncasso attuale: {tot} €\nIncasso stimato: > {potenziale} €\n")
 
 
 
@@ -49,4 +55,5 @@ e1.prenota(10)
 print(e1.posti_disponibili())
 e1.annulla_prenotazione(5)
 print(e1.posti_disponibili())
-
+(e1.calcola_incasso())
+e1.ottieni_info_evento()
