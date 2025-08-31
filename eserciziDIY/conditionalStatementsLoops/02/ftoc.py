50a00fa770afd602c41f524f7a6d7ae2451b9617
xxxNdr <andrea1987andre@gmail.com>
2025-08-31 20:31:35 +0200

    es2 migliorato

--- eserciziDIY/conditionalStatementsLoops/02/ftoc.py
+++ eserciziDIY/conditionalStatementsLoops/02/ftoc.py
@@ -11,9 +11,9 @@ convenzione = temp[-1]
 
 if convenzione == "c":
     fahre = (gradi * 9 / 5) + 32
-    print(f"{round(fahre, 2)}F")
+    print(f"{round(fahre, 2)}°F")
 elif convenzione == "f":
     cel = (gradi - 32) * 5 / 9
-    print(f"{round(cel, 2)}C°")
+    print(f"{round(cel, 2)}°C")
 else:
     print("unità di misira non riconosciuta")
